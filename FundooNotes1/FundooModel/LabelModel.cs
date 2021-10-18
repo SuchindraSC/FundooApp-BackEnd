8f127775efafdb39b71ed613fb45d59106bfca97
SuchindraSC <suchindrachitnis@gmail.com>
2021-10-18 15:19:47 +0530

    [Suchindra Chitnis]Refactor: Added Label Model

---
+++ FundooNotes1/FundooModel/LabelModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace FundooModel
+{
+    public class LabelModel
+    {
+        [Key]
+        public int LabelId { get; set; }
+
+        [ForeignKey("NotesModel")]
+        public int? NotesId { get; set; }
+        public virtual NotesModel NotesModel { get; set; }
+
+        [ForeignKey("UserModel")]
+        public int UserId { get; set; }
+        public virtual UserModel UserModel { get; set; }
+
+        [Required]
+        public string LabelName { get; set; }
+    }
+}
---
+++ FundooNotes1/FundooRepository/Migrations/20211018094827_Labels.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FundooRepository.Migrations
+{
+    public partial class Labels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Labels",
+                columns: table => new
+                {
+                    LabelId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NotesId = table.Column<int>(nullable: true),
+                    UserId = table.Column<int>(nullable: false),
+                    LabelName = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Labels", x => x.LabelId);
+                    table.ForeignKey(
+                        name: "FK_Labels_Notes_NotesId",
+                        column: x => x.NotesId,
+                        principalTable: "Notes",
+                        principalColumn: "NotesId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Labels_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Labels_NotesId",
+                table: "Labels",
+                column: "NotesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Labels_UserId",
+                table: "Labels",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Labels");
+        }
+    }
+}
